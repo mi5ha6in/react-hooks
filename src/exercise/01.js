693720da9b9f1668332efe183049a3256b1eb623
mi5ha6in <mi5ha6in@gmail.com>
2022-11-07 23:38:00 +0300

    adds task 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,17 +5,18 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState('Ivan');
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input defaultValue={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
