a9c7ebc7247b92c82a33645fd19896b46f327e18
mi5ha6in <mi5ha6in@gmail.com>
2022-11-16 18:38:48 +0300

    adds task 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,27 +6,37 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
+  const [pokemon, setPokemon] = React.useState(null)
+
+  React.useEffect(() => {
+    const getPokemon = async () => {
+      if (pokemonName) {
+        setPokemon(null)
+        const pokemonInfo = await fetchPokemon(pokemonName)
+        setPokemon(pokemonInfo)
+      }
+    }
+    getPokemon()
+  }, [pokemonName])
+
+  const info = pokemon ? (
+    <PokemonDataView pokemon={pokemon} />
+  ) : pokemonName ? (
+    <PokemonInfoFallback name={pokemonName} />
+  ) : (
+    'Submit a pokemon'
+  )
   // 💣 remove this
-  return 'TODO'
+  return <>{info}</>
 }
 
 function App() {
