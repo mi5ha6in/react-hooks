789af4b850f7575fcf80533081012618823d94f9
mi5ha6in <mi5ha6in@gmail.com>
2022-11-18 01:40:20 +0300

    adds task 6, extra 5

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -100,7 +100,7 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary>
+        <ErrorBoundary key={pokemonName}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
